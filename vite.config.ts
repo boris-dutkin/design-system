419f7b37e16b17fb369c56559cd3bc6e0da31c7c
boris-dutkin <boris.webdev@gmail.com>
2024-05-12 17:25:36 +0300

    build(configuration): configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/components/index.ts'),
+      name: 'DesignSystem',
+      fileName: 'design-system',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss',
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+  optimizeDeps: {
+    include: ['design-system'],
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+});
